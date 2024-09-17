ed2f99820d64f5a37cd0ea88545b09378e92eb6b
IOSdeVeloper20 <81517945+IOSdeVeloper20@users.noreply.github.com>
2024-09-17 20:09:58 +0300

    Approvals Page Done

---
+++ src/Pages/Approvals/ApprovalBottom.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Roofman from "../../Assets/Images/roof_man.jpeg";
+
+const ApprovalBottom = () => {
+  return (
+    <div>
+      <img src={Roofman} alt="Roof Man" className="rounded-t-3xl" />
+    </div>
+  );
+};
+
+export default ApprovalBottom;
--- src/Pages/Approvals/index.jsx
+++ src/Pages/Approvals/index.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer";
+import ApprovalHeader from "./ApprovalHeader";
+import ApprovalBottom from "./ApprovalBottom";
 
 const Approvals = () => {
   return (
     <div>
       <Navbar />
-      Approvals Page
+      <div className="rounded-4xl bg-darkGrey p-4 mb-4">
+        <div className="rounded-4xl bg-white">
+          <ApprovalHeader />
+        </div>
+      </div>
+      <ApprovalBottom />
       <Footer />
     </div>
   );
